69d883747581629d6fd292fff23f90c61168ed6c
leoniedunnett <leoniedunnett@ymail.com>
2015-10-21 22:04:16 +1100

    commented out removed

--- Lesson 8/faq/js/script.js
+++ Lesson 8/faq/js/script.js
@@ -1,41 +1,41 @@
 // Step 1: show #answer1 on user click
 
-// function showAnswer1() {
-//     $('#answer1').show();
-// }
-// $('#answer1').hide();
-// $('#question1').click(showAnswer1);
+function showAnswer1() {
+    $('#answer1').show();
+}
+$('#answer1').hide();
+$('#question1').click(showAnswer1);
 
 
 
 // Step 2: toggle #answer1 on user click
 
-// function toggleAnswer1() {
-//     $('#answer1').toggle();
-// }
-// $('#question1').click(toggleAnswer1);
+function toggleAnswer1() {
+    $('#answer1').toggle();
+}
+$('#question1').click(toggleAnswer1);
 
 
 
 // Step 3: slideToggle #answer1 on user click
 
-// function toggleAnswer1() {
-//   $('#answer1').slideToggle();
-// }
-// $('#question1').click(toggleAnswer1);
+function toggleAnswer1() {
+  $('#answer1').slideToggle();
+}
+$('#question1').click(toggleAnswer1);
 
 
 
 // Step 4: slideToggle #answer1 and #answer2 on user click
 
-// function toggleAnswer1() {
-//   $('#answer1').slideToggle();
-// }
-// function toggleAnswer2() {
-//   $('#answer2').slideToggle();
-// }
-// $('#question1').click(toggleAnswer1);
-// $('#question2').click(toggleAnswer2);
+function toggleAnswer1() {
+  $('#answer1').slideToggle();
+}
+function toggleAnswer2() {
+  $('#answer2').slideToggle();
+}
+$('#question1').click(toggleAnswer1);
+$('#question2').click(toggleAnswer2);
 
 
 
