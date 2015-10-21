aa27e9d17fcbc2f849d957d71b8ae97e214547ce
leoniedunnett <leoniedunnett@ymail.com>
2015-10-21 21:53:10 +1100

    Updated solution to better match student learning point

--- Lesson 8/color_scheme_switcher/js/index.js
+++ Lesson 8/color_scheme_switcher/js/index.js
@@ -1,23 +1,28 @@
 $('#grayButton').click(function(){
-    // switchOne();
-    switchColor('gray');
+    switchOne();
+    // switchColor('gray');
 });
 
 $('#whiteButton').click(function(){
-    switchColor('white');
+    switchTwo();
+    // switchColor('white');
 });
 
-// Example 1
-function switchOne() {
-    $("body").css({"background-color":"gray","color":"white"});
-}
+// // Example 1
+// function switchOne() {
+//     $("body").css({"background-color":"gray","color":"white"});
+// }
 
 // Example 2
 function switchOne() {
     $('body').attr('class', 'gray');
 }
 
-// Example 3
-function switchColor(colorName){
-    $('body').attr('class', colorName);
+function switchTwo() {
+    $('body').attr('class', 'white');
 }
+
+// Advanced Example 3
+// function switchColor(colorName){
+//     $('body').attr('class', colorName);
+// }
